4d5141c7dffb5f4f3fa89d21d0421ae26f8c22ad
vik-wed <wedel.viktoria@gmail.com>
2024-03-18 18:04:28 -0400

    Update homepage styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,14 +7,11 @@ import theme from "../styles/theme"
 import { mdiGithub, mdiInstagram, mdiLinkedin } from "@mdi/js"
 import Icon from "@mdi/react"
 import Image from "next/image"
-import { usePathname, useRouter } from "next/navigation"
+import { useRouter } from "next/navigation"
 import styled, { keyframes } from "styled-components"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname()
   const router = useRouter()
-
-  const isHomepage = pathname === "/"
 
   return (
     <html lang="en">
@@ -26,7 +23,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       </head>
       <StyleProvider>
         <body>
-          <Logo $homeAnimation={isHomepage}>
+          <Logo>
             <Image src="/logo-dark.png" height={200} width={200} alt="Qualle Tech Logo" />
           </Logo>
           <Container>
@@ -36,15 +33,18 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               </IconContainer>
               {children}
             </Main>
+            <SubTitle>
+              Qualle <i>noun</i> - /kwɑːl.i/ quall-e - jellyfish
+            </SubTitle>
             <SideBar>
               <IconContainer href="https://www.linkedin.com/in/vikwedel/" target="__blank" rel="noopener noreferrer">
-                <Icon path={mdiLinkedin} size="2rem" color={theme.colors.secondaryDark} />
+                <Icon path={mdiLinkedin} size="2rem" color={theme.colors.primaryLight} />
               </IconContainer>
               <IconContainer href="https://www.instagram.com/qualle.tech" target="__blank" rel="noopener noreferrer">
-                <Icon path={mdiInstagram} size="2rem" color={theme.colors.secondaryDark} />
+                <Icon path={mdiInstagram} size="2rem" color={theme.colors.primaryLight} />
               </IconContainer>
               <IconContainer href="https://github.com/vik-wed" target="__blank" rel="noopener noreferrer">
-                <Icon path={mdiGithub} size="2rem" color={theme.colors.secondaryDark} />
+                <Icon path={mdiGithub} size="2rem" color={theme.colors.primaryLight} />
               </IconContainer>
             </SideBar>
           </Container>
@@ -54,58 +54,50 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   )
 }
 
-const homepagefloatAnimation = keyframes`
-  0% {
-    transform: translatey(0px);
-  }
-  15% {
-    transform: translatey(-150px) translatex(-173px);
-  }
-  25% {
-    transform: translatey(-250px) translatex(-250px);
-  }
-  50% {
-    transform: translatey(-300px) translatex(-600px);
-  }
-  75% {
-    transform: translatey(-150px) translatex(-750px);
-  }
-  100% {
-    transform: translatey(0px);
-  }
-`
-
 const floatAnimation = keyframes`
   0% {
     transform: translatey(0px);
   }
   50% {
-    transform: translatey(-20px);
+    transform: translatey(-2rem);
    }
   100% {
     transform: translatey(0px);
   }
 `
 
-const Logo = styled.div<{ $homeAnimation: boolean }>`
+const Logo = styled.div`
   position: absolute;
-  bottom: 30px;
-  right: 50px;
+  bottom: 2.5rem;
+  right: 5rem;
   opacity: 60%;
-  animation-name: ${({ $homeAnimation }) => ($homeAnimation ? homepagefloatAnimation : floatAnimation)};
-  animation-duration: ${({ $homeAnimation }) => ($homeAnimation ? "60s" : "5s")};
+  z-index: 10;
+  animation-name: ${floatAnimation};
+  animation-duration: 5s;
   animation-iteration-count: infinite;
   animation-direction: alternate;
 `
 
 const Container = styled.div`
+  background: ${theme.colors.backgroundDark};
+  height: 100%;
   display: grid;
+  border-radius: 0.5rem;
   grid-template-columns: 95% 5%;
   padding: 2rem;
   grid-gap: 1rem;
-  height: 100vh;
-  width: 100vw;
   overflow: none;
+`
+
+const SubTitle = styled.p`
+  color: ${theme.colors.primaryLight};
+  font-size: 1.7rem;
+  width: 90vh;
+  position: fixed;
+  left: 1.2rem;
+  bottom: 0;
+  transform-origin: 0 0;
+  transform: rotate(-90deg);
 `
 
 const SideBar = styled.div`
@@ -115,7 +107,7 @@ const SideBar = styled.div`
   height: 90vh;
   position: fixed;
   right: 1rem;
-  top: 1rem;
+  top: 5vh;
 `
 
 const IconContainer = styled.a`
--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,25 @@
 "use client"
 
+import theme from "../styles/theme"
 import { useRouter } from "next/navigation"
 import styled, { keyframes } from "styled-components"
 
 export default function Page() {
   const router = useRouter()
   return (
-    <Container>
-      <SubTitle>
-        Qualle <i>noun</i> - /kwɑːl.i/ quall-e - jellyfish
-      </SubTitle>
-      <Columns>
-        <Blob onClick={() => router.push("/projects")} $background="/blob1.svg" $margin="-20">
-          <h3>Developed by us</h3>
-        </Blob>
-        <Blob onClick={() => router.push("/about")} $background="/blob2.svg" $margin="30">
-          <h3>About us</h3>
-        </Blob>
-        <Blob onClick={() => router.push("/contact")} $background="/blob3.svg" $margin="20">
-          <h3>Work with us</h3>
-        </Blob>
-      </Columns>
-    </Container>
+    <Columns>
+      <Blob onClick={() => router.push("/projects")} $background="/blob1.svg" $margin="-30">
+        <h3>Developed by us</h3>
+      </Blob>
+      <Blob onClick={() => router.push("/about")} $background="/blob2.svg" $margin="60">
+        <h3>About us</h3>
+      </Blob>
+      <Blob onClick={() => router.push("/contact")} $background="/blob3.svg" $margin="-100">
+        <h3>Work with us</h3>
+      </Blob>
+    </Columns>
   )
 }
-
-const Container = styled.div`
-  display: grid;
-  grid-gap: 1rem;
-  grid-template-rows: auto 1fr;
-`
-
-const SubTitle = styled.h2`
-  font-size: 1rem;
-  padding: 0 10rem;
-`
 
 const Columns = styled.div`
   display: grid;
@@ -48,7 +33,7 @@ const floatAnimation = keyframes`
     transform: translatey(0px);
   }
   50% {
-    transform: translatey(-10px);
+    transform: translatey(-10px) scale(0.9);
    }
   100% {
     transform: translatey(0px);
@@ -62,6 +47,8 @@ const Blob = styled.div<{ $background: string; $margin: string }>`
   justify-content: center;
   align-items: center;
   background: ${({ $background }) => `url(${$background})`} 0 0/105% 100% no-repeat;
+  filter: drop-shadow(0 0.2rem 0.7rem #21219b40);
+  z-index: 20;
   &:hover {
     opacity: 80%;
     animation-name: ${floatAnimation};
