2c995dbe1fa0b5165c966f52b5e2c0d9a3403821
vik-wed <wedel.viktoria@gmail.com>
2024-04-19 12:23:29 -0400

    Add title

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
+        <title>Qualle Tech</title>
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@700&family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap"
