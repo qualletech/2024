a7dbe149fa589818294fda63be2c4efd2478dc41
vik-wed <wedel.viktoria@gmail.com>
2024-04-26 10:51:48 -0400

    Add meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,17 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@700&family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap"
         />
+        <meta
+          name="description"
+          content="Qualle Tech provides custom web development services to organizations and individuals."
+        />
+        <meta
+          name="keywords"
+          content="web development, website, web application, full stack development, service provider, web design, web dev"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta name="author" content="Vik Wedel" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <StyleProvider>
         <body>
