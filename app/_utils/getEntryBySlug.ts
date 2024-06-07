3b50711b539dd8f1c568e58298bc871a73dbd061
vik-wed <wedel.viktoria@gmail.com>
2024-06-07 15:26:52 -0400

    Update type

--- app/_utils/getEntryBySlug.ts
+++ app/_utils/getEntryBySlug.ts
@@ -1,6 +1,6 @@
 import contentfulClient from "./contentfulClient"
 
-export default async function getEntryBySlug(slug: string, type: string) {
+export default async function getEntryBySlug(slug: string, type: string): Promise<any> {
   const queryOptions = {
     content_type: type,
     "fields.slug[match]": slug,
