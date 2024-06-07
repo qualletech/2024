dd53939eedb4fe19342f593b61d840d9c69f2841
vik-wed <wedel.viktoria@gmail.com>
2024-06-07 16:51:35 -0400

    Update blog page

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,17 +1,35 @@
-import { createContentClient, getEntryBySlug } from "../../_utils/contentful"
+import PageContainer from "../../_components/PageContainer"
+import getEntryBySlug from "../../_utils/getEntryBySlug"
+import getSlugsForParams from "../../_utils/getSlugsForParams"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
-const client = createContentClient()
-
 export async function generateStaticParams() {
-  const queryOptions = {
-    content_type: "blog",
-    select: "fields.slug",
-  }
-  const articles = await client.getEntries(queryOptions)
-  return articles.items.map((article) => ({
-    slug: article.fields.slug,
-  }))
+  const slugs = await getSlugsForParams("blog")
+  return slugs
+}
+
+const renderOptions = {
+  renderNode: {
+    "embedded-asset-block": (node) => {
+      const { file, title } = node.data.target.fields
+      const { url } = file
+
+      return (
+        <img
+          src={url}
+          alt={title}
+          style={{
+            maxHeight: "30dvh",
+            maxWidth: "90%",
+            aspectRatio: "auto",
+            objectFit: "contain",
+            borderRadius: "1rem",
+          }}
+        />
+      )
+    },
+    paragraph: (node, children) => <p style={{ width: "90%", wordBreak: "break-word" }}>{children}</p>,
+  },
 }
 
 export default async function BlogPage(props) {
@@ -22,9 +40,9 @@ export default async function BlogPage(props) {
   const { title, content } = blog.fields
 
   return (
-    <div>
+    <PageContainer>
       <h3>{title}</h3>
-      {documentToReactComponents(content)}
-    </div>
+      {documentToReactComponents(content, renderOptions)}
+    </PageContainer>
   )
 }
