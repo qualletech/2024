35f7ff88297c28e4519e32accfbba13acb4d6a35
vik-wed <wedel.viktoria@gmail.com>
2024-06-07 14:14:07 -0400

    Update project page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,9 +1,7 @@
-"use client"
-
 import Card from "../_components/Card"
+import Columns from "../_components/Columns"
 import PageContainer from "../_components/PageContainer"
 import { PROJECTS } from "./CONSTANTS"
-import styled from "styled-components"
 
 export default function Page() {
   return (
@@ -13,18 +11,3 @@ export default function Page() {
     </PageContainer>
   )
 }
-
-const Columns = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  align-content: start;
-  grid-gap: 1rem;
-  overflow: auto;
-  padding-right: 2rem;
-  @media screen and (max-width: 1023px) and (orientation: portrait) {
-    grid-template-columns: unset;
-  }
-  @media screen and (max-width: 769px) and (orientation: landscape) {
-    grid-template-columns: 1fr 1fr;
-  }
-`
